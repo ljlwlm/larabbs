d951a2e69ef95eeaae1963375f531e4b76d05aa4
vagrant <vagrant@homestead>
2018-05-26 09:01:09 +0000

    User can upload avatar

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -4,17 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Auth;
+
 class UserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        return false;
+        return true;
     }
+
 
     /**
      * Get the validation rules that apply to the request.
@@ -27,12 +24,16 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
+
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 200px 以上',
             'name.unique' => '用户名已被占用，请重新填写',
             'name.regex' => '用户名只支持英文、数字、横杆和下划线。',
             'name.between' => '用户名必须介于 3 - 25 个字符之间。',
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
